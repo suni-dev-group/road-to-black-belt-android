19517d44fe85ad2484cc6db5ca8dff31b50a8001
kyu_chan <41310752+kyuchanE@users.noreply.github.com>
2025-02-13 20:58:51 +0900

    init project

---
+++ workspace/app/src/androidTest/java/com/codesubmission/road_to_black_belt/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.codesubmission.road_to_black_belt
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.codesubmission.road_to_black_belt", appContext.packageName)
+    }
+}
---
+++ workspace/settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Road_To_Black_Belt"
+include(":app")
+include(":core:data")
+include(":core:domain")
+include(":core:ui")
+include(":feature:main")
+include(":navigator")
+include(":core:testing")
